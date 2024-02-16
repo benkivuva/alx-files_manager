bbe5e634f2aeb00835d4dc7bda3df3a4598e22ee
benkivuva <benkivuvak5@gmail.com>
2024-02-16 00:44:45 -0800

    add authenticate endpoints

--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,8 @@ const router = express.Router();
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
+router.get('/connect', AuthController.getConnect);
+router.get('/disconnect', AuthController.getDisconnect);
+router.get('/users/me', UsersController.getMe);
 router.post('/users', UsersController.postNew);
 module.exports = router;
