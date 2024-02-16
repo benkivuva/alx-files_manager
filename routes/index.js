9658a60e7250bb6ca0f8d4acd82e1edc359e2d81
benkivuva <benkivuvak5@gmail.com>
2024-02-16 00:54:10 -0800

    add endpoint for uploading files

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ import express from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
 import AuthController from '../controllers/AuthController';
+import FilesController from '../controllers/FilesController';
 
 const router = express.Router();
 
@@ -12,4 +13,6 @@ router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
 router.get('/users/me', UsersController.getMe);
 router.post('/users', UsersController.postNew);
+router.post('/files', FilesController.postUpload);
+
 module.exports = router;
